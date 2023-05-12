0fdebde60b76a026a658879ea30d6929f642f150
Juhwan Kim <120164895+Juhwan0687@users.noreply.github.com>
2023-05-12 17:29:28 +0900

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,16 +1,1 @@
-from tkinter import*
 
-win=Tk()
-win.title('환경 시뮬레이터')
-win.geometry('600x600')
-
-f1=Frame(win,bg='white')
-b1=Button(f1,text='Go f2',command=lambda:[f2.tkraise()])
-f2=Frame(win,bg='blue')
-b2=Button(f2,text='Go f1',command=lambda:[f1.tkraise()])
-
-f1.grid(row=0,column=0)
-f2.grid(row=0,column=0)
-b1.pack()
-b2.pack()
-win.mainloop()
